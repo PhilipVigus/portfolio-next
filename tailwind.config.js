c56a82225143a5246f1fcae9c2cf79e50c8575ce
Phil Vigus <philvigus@gmail.com>
2024-07-04 18:01:19 +0200

    Play around with the styles a bit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,3 @@
-const colors = require('tailwindcss/colors');
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
     content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
     daisyui: {
