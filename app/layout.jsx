cb7b24aeda19487decfb7231bcd6d189ce3875dc
Phil Vigus <philvigus@gmail.com>
2024-07-04 18:00:57 +0200

    Play around with the styles a bit

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,5 @@
 import '../styles/globals.css';
-import { Footer } from '../components/footer';
-import { Header } from '../components/header';
+import '../styles/main.css';
 
 export const metadata = {
     title: {
@@ -11,19 +10,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
     return (
-        <html lang="en" data-theme="lofi">
-            <head>
-                <link rel="icon" href="/favicon.svg" sizes="any" />
-            </head>
-            <body className="antialiased text-white bg-blue-900">
-                <div className="flex flex-col min-h-screen px-6 bg-grid-pattern sm:px-12">
-                    <div className="flex flex-col w-full max-w-5xl mx-auto grow">
-                        <Header />
-                        <div className="grow">{children}</div>
-                        <Footer />
-                    </div>
-                </div>
-            </body>
+        <html lang="en" data-theme="mytheme">
+        <head>
+            <link rel="icon" href="/favicon.svg" sizes="any" />
+            <title>Phil&apos;s Portfolio</title>
+        </head>
+        <body className="screen">
+        <div className="grow">{children}</div>
+
+        </body>
         </html>
     );
 }
---
+++ app/test/page.jsx
@@ -0,0 +1,12 @@
+import '../../styles/main.css';
+
+export default function Page() {
+
+    return (
+        <div>
+            <pre className="pre">Blah</pre>
+            <div className="interlaced"></div>
+            <div className="glare"></div>
+        </div>
+    );
+}
