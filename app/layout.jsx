316b8bd549ed5109d1b32cae62a481ba61f3a24a
Phil Vigus <philvigus@gmail.com>
2024-07-06 13:06:11 +0200

    Get the screen looking right again

--- app/layout.jsx
+++ app/layout.jsx
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
             <link href="https://fonts.googleapis.com/css2?family=Ga+Maamli&display=swap" rel="stylesheet" />
         </head>
-        <body className="screen">
+        <body>
         <div className="grow">{children}</div>
 
         </body>
--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,20 +1,18 @@
 'use client';
 
 import '../../styles/main.css';
-import useGridSize from '../../hooks/useGridSize';
 
 export default function Page() {
-    const { columns, rows } = useGridSize();
-    console.log(columns, rows);
     return (
-        <div className="min-h-screen">
-            <div
-                className="min-h-screen grid grid-cols-repeat-auto-fill grid-rows-repeat-auto-fill leading-char tracking-char">
-                <div className="col-start-55 row-start-31 col-span-1 row-span-1">D</div>
-
-                <div className="interlaced"></div>
-                <div className="glare"></div>
+        <div className="h-full screen">
+            <div className="h-screen flex flex-col items-center justify-center p-40">
+                <div className="w-full h-full flex justify-center items-center box-border bg-transparent z-50">
+                    BLAH
+                    <div className="interlaced"></div>
+                    <div className="glare"></div>
+                </div>
             </div>
+
         </div>
     );
 }
