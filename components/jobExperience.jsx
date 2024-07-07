df9d7ba26c9513a4880f1e9b8c186410a85b267b
Phil Vigus <philvigus@gmail.com>
2024-07-07 11:51:49 +0200

    Add company and date information

--- components/jobExperience.jsx
+++ components/jobExperience.jsx
@@ -3,7 +3,13 @@ import { SubHeading } from "./subHeading";
 export default function JobExperience({ job }) {
   return (
     <div key={job.company}>
-      <SubHeading title={job.title} />
+      <div className="flex justify-between">
+        <div className="flex gap-2">
+          <SubHeading title={job.title} />
+          <div>({job.company})</div>
+        </div>
+        <div>{job.date}</div>
+      </div>
       <p>{job.description}</p>
       <ul>
         <div>Key achievements:</div>
