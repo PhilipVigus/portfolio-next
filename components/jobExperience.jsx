4a27ce83d99f9f8ecf4649c7a24af86b337a3238
Phil Vigus <philvigus@gmail.com>
2024-07-07 12:16:52 +0200

    Remove unnecessary attribute

--- components/jobExperience.jsx
+++ components/jobExperience.jsx
@@ -2,7 +2,7 @@ import { SubHeading } from "./subHeading";
 
 export default function JobExperience({ job }) {
   return (
-    <div key={job.company}>
+    <div>
       <div className="flex justify-between">
         <SubHeading title={job.title} />
         <div>{job.date}</div>
